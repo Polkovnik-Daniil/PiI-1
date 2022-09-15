82109016419db0940088aa43065cf3e92fa2fe7e
Polkovnik-Daniil <0205danik@mail.ru>
2022-09-15 09:55:55 +0300

    version 0.6

--- WebApplication/WebApplication/TimeHandler.cs
+++ WebApplication/WebApplication/TimeHandler.cs
@@ -1,33 +1,45 @@
 ﻿using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
 using System.Web;
+using System.Web.WebSockets;
 
 namespace WebApplication {
     public class TimeHandler : IHttpHandler {
-        /// <summary>
-        /// Вам потребуется настроить этот обработчик в файле Web.config вашего 
-        /// веб-сайта и зарегистрировать его с помощью IIS, чтобы затем воспользоваться им.
-        /// см. на этой странице: https://go.microsoft.com/?linkid=8101007
-        /// </summary>
-        #region Члены IHttpHandler
-
+        WebSocket socket;
         public bool IsReusable {
-            // Верните значение false в том случае, если ваш управляемый обработчик не может быть повторно использован для другого запроса.
-            // Обычно значение false соответствует случаю, когда некоторые данные о состоянии сохранены по запросу.
-            get { return true; }
+            get { return false; }
         }
 
         public void ProcessRequest(HttpContext context) {
-            //разместите здесь вашу реализацию обработчика.
-            try {
-                HttpRequest request = context.Request;
-                HttpResponse response = context.Response;
-                string value = request.QueryString["Value"];
-                response.Write($"<h1>{DateTime.Now}</h1>");
-            } catch {
-                context.Response.Redirect("/TimeHandler.html");
+            if (context.IsWebSocketRequest) {
+                context.AcceptWebSocketRequest(WebSocketRequest);
             }
         }
+        private async Task WebSocketRequest(AspNetWebSocketContext context) {
+            socket = context.WebSocket;
+            string s = await Receive();
+            await Send(s);
+            while (socket.State == WebSocketState.Open) {
+                Thread.Sleep(2000);
+                await Send(DateTime.Now.ToString());
+            }
+            await socket.CloseAsync(WebSocketCloseStatus.Empty, "12", CancellationToken.None);
+        }
 
-        #endregion
+        private async Task<string> Receive() {
+            string rc = null;
+            var buffer = new ArraySegment<byte>(new byte[512]);
+            var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+            rc = System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+            return rc;
+        }
+
+        private async Task Send(string s) {
+            var sendbuffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes("response:" + s));
+            await socket.SendAsync(sendbuffer, WebSocketMessageType.Text,
+                                   true, CancellationToken.None);
+        }
     }
 }
